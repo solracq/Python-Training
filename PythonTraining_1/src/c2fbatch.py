967dc9942491d8b20e8a6a2f4cb9dd8848bc2674
Solrac <Solrac@192.168.0.10>
2013-11-10 13:28:56 -0500

    First Commit

---
+++ PythonTraining_1/src/c2fbatch.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 24, 2013
+
+@author: cquiroz
+'''
+
+import sys
+
+if __name__ == '__main__':
+    pass
+
+celsius = float(sys.argv[1])
+
+fahrenheit = (9*celsius)/5 +32
+
+print(celsius,"\u00B0 Celsius corresponds to ", fahrenheit, "\u00B0 Fahrenheit")
+
---
+++ PythonTraining_2/currmonth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+
+@author: jstrick
+Created on Wed Mar 20 21:27:45 2013
+
+"""
+import calendar
+tc = calendar.TextCalendar()
+tc.prmonth(2013, 3)
