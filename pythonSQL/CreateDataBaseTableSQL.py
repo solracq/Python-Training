c806852485f921b774314f405b1b6e345ba6f51a
Carlos Quiroz <solracq@gmail.com>
2020-05-03 18:52:07 -0400

    Executing SQL with python

---
+++ pythonSQL/CreateDataBaseTableSQL.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+class CreateDB:
+
+    def __init__(self, database):
+        self.database = database
+        self.conn = sqlite3.connect(self.database)
+        self.cur = self.conn.cursor()
+
+    def createTable(self, table):
+        self.cur.execute("DROP TABLE IF EXISTS " + table)
+        self.cur.execute('CREATE TABLE ' + table + ' (id TEXT, fullName TEXT, gender TEXT, bday DATE)')
+
+    def insertData(self, id, fullName, gender, bday):
+        self.cur.execute('INSERT INTO UserRecords (id, fullName, gender, bday) VALUES (?, ?, ?, ?)', (id, fullName, gender, bday))
+        self.conn.commit()
+
+    def closeDB(self):
+        self.conn.close()
+
+    def displayTable(self, table, sorted, columnNum):
+        if sorted == True:
+            self.cur.execute('SELECT * FROM ' + table + ' ORDER BY ' + str(columnNum) + ' ASC')
+        else:
+            self.cur.execute('SELECT * FROM ' + table)
+        for row in self.cur:
+            print(row)
---
+++ pythonSQL/GroupUsers.py
@@ -0,0 +1,28 @@
+import _sqlite3
+from CreateDataBaseTableSQL import CreateDB
+
+def create_users_database_table():
+    sql_database = CreateDB('userRecords.sqlite')
+    sql_database.createTable('UserRecords')
+
+    sql_database.insertData('PID1', 'POND^AMY', 'F', 19890224)
+    sql_database.insertData('PID2', 'WILLIAMS^RORY', 'M', 19881102)
+    sql_database.insertData('PID3', 'POND^AMY', 'F', 19890224)
+    sql_database.insertData('PID4', 'CLARA^OSWALD', 'F', 19890224)
+    sql_database.insertData('PID5', 'POND^AMY', 'F', 20010911)
+    sql_database.insertData('PID6', 'CLAR^OSWALD', 'F', 19890224)
+    sql_database.insertData('PID7', 'POND^AMELIA', 'F', 20010911)
+    sql_database.insertData('PID8', 'CLARA^oswald', 'F', 19890224)
+    sql_database.insertData('PID9', 'TYLER^ROSE', 'F', 20000101)
+    sql_database.insertData('PID10', 'NOBLE^DONNA', 'F', 19780405)
+    sql_database.insertData('PID11', 'TYLER^ROSE', 'F', 20000101)
+    sql_database.insertData('PID12', 'NOBLE^DONN', 'F', 19780405)
+    sql_database.insertData('PID13', 'TYLER^ROS', 'F', 20000102)
+    sql_database.insertData('PID14', 'CLARA^OSWALD^COLEMAN', 'F', 19890224)
+
+    sql_database.displayTable('UserRecords', True, 2)
+
+    sql_database.closeDB()
+
+if __name__ == "__main__":
+    create_users_database_table()
